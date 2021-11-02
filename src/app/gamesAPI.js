af77569e261d9a6ef66a8f3ca950e49d9530932b
aymaneBOUJELLABA <aymane.boujellaba@gmail.com>
2021-11-02 21:19:53 +0100

    added vercel.json

--- src/app/gamesAPI.js
+++ src/app/gamesAPI.js
@@ -3,6 +3,7 @@ export const fetchVideoGames = async() =>
     const response = await fetch(
         process.env.REACT_APP_API+'videogames?sort=id',
         {
+            mode : 'cors',
             method : 'GET',
             headers : {
                 'Content-Type' : 'application/json',
